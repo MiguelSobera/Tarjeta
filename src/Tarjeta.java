37c1890c901cd89aa8b4c3835f761b7751129a48
felix <felix@192.168.56.1>
2020-02-05 18:27:59 +0100

    Subida de la nueva clase tarjeta con los metotodos saldo y dni ademas de el constructor y las variables

---
+++ src/Tarjeta.java
@@ -0,0 +1,25 @@
+
+public class Tarjeta implements ITarjetaMonedero, ITarjetaDebito {
+	private String id;
+	private String dniTitular;
+	private int pin;
+	private float saldo;
+
+	public Tarjeta(String id, String dniTitular, int pin, float saldo) {
+		setId(id);
+		setDniTitular(dniTitular);
+		setPin(pin);
+		setSaldo(saldo);
+
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+
+	}
+
+	public void setDniTitular(String dniTitular) {
+		this.dniTitular = dniTitular;
+
+	}
+}
