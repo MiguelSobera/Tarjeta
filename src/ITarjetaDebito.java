446fc9b69426c9fa28e9388aaa962d54c351073f
CarlaValverde <carla.maria.val@gmail.com>
2020-02-05 17:39:26 +0100

    Subida de la tarea #2, implementación de una interfaz.

---
+++ src/ITarjetaDebito.java
@@ -0,0 +1,7 @@
+
+public interface ITarjetaDebito {
+
+	public void comprar(float importe, String dni);
+	
+	public void retirarCajero(float importe, int pin);
+}
--- src/TarjetaDebito.java
+++ src/TarjetaDebito.java
@@ -2,7 +2,7 @@
 public class TarjetaDebito {
 
 	public static void main(String[] args) {
-		System.out.println("Buenas Tardes");
+		System.out.println("Proyecto tarjetas");
 		
 
 	}
