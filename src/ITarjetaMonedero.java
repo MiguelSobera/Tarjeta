ffd89ef9d6d52e73a2121c7dbf62d6710aff7810
CarlaValverde <carla.maria.val@gmail.com>
2020-02-05 17:49:01 +0100

    Creación de la interfaz ITarjetaMonedero, subida de la tarea #8.

---
+++ src/ITarjetaMonedero.java
@@ -0,0 +1,13 @@
+
+public interface ITarjetaMonedero {
+
+	public void comprar(float importe, String dni);
+
+	public String getId();
+
+	public String getDniTitular();
+
+	public int getSaldo();
+
+	public void setPin();
+}
