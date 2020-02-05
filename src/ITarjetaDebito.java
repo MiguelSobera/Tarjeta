d95acc300554331ba542e1d17cc21c7611110bb4
blanc <blanc@LAPTOP-PGBGGRV3>
2020-02-05 17:44:09 +0100

    Actualización de la interfaz tarjeta débito, tarea #7

--- src/ITarjetaDebito.java
+++ src/ITarjetaDebito.java
@@ -4,4 +4,13 @@ public interface ITarjetaDebito {
 	public void comprar(float importe, String dni);
 	
 	public void retirarCajero(float importe, int pin);
+	
+	public String getId();
+	
+	public String getDniTitular();
+	
+	public int getSalgo();
+	
+	public void setPin(int iPin);
+	
 }
