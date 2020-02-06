b853eff7b2b5ca1a1abf78d9a64db558c481b859
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 12:18:17 +0100

    Añadir un comentario en el main

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,9 @@
 
 public class Principal {
-	
+		//Declaración de los datos en al main
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
-		System.out.println("Pagando con tarjeta...");
-		tarjeta.comprar(100, "29556545T");
-		System.out.println("Importe Foster Hollywood 100€...");
-		System.out.println("");
-		System.out.println("El saldo actual del monedero es: " + monedero.getSaldo());
-		System.out.println("Pagando con monedero...");
+	
 }
 }
