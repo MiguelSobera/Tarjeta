635226a60fa05010ad025e5aa0dd83f721cb5112
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 12:53:26 +0100

    He cerrado la tarea 3. Close #3

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 
 public class Principal {
-	//Declaracion de los datos en el main
+	//Declaracion de los datos del main
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
