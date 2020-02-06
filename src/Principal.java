63289284da810f7bf83c652ea03d22199bce1d77
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 12:20:09 +0100

    Añadido de un comentario para la tarea 8

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
 
 public class Principal {
-		//Declaración de los datos en al main
+	//Declaracion de los datos en el main
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
-	
-}
+		//Aclaracion del pago
+		System.out.println("Pagando con tarjeta...");
+		tarjeta.comprar(100, "29556545T");
+
 }
