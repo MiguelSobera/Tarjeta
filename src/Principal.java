4ec38eb7da548b17c55ca8d841e5d71bb542a7e8
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 13:07:31 +0100

    Fin de la tarea 3 close #3

--- src/Principal.java
+++ src/Principal.java
@@ -4,11 +4,5 @@ public class Principal {
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
-		System.out.println("Pagando con tarjeta...");
-		tarjeta.comprar(100, "29556545T");
-		System.out.println("Importe Foster Hollywood 100€...");
-		System.out.println("");
-		System.out.println("El saldo actual del monedero es: " + monedero.getSaldo());
-		System.out.println("Pagando con monedero...");
 }
 }
