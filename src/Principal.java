a7ce835abea222b9c500ee3dca90b63003ff1c13
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 12:52:39 +0100

    Close #3

--- src/Principal.java
+++ src/Principal.java
@@ -4,10 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
-		//Aclaracion del pago
 		System.out.println("Pagando con tarjeta...");
 		tarjeta.comprar(100, "29556545T");
-		//Se termina de implementar datos en el main
 		System.out.println("Importe Foster Hollywood 100€...");
 		System.out.println("");
 		System.out.println("El saldo actual del monedero es: " + monedero.getSaldo());
