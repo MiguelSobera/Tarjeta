7824e44d97c92bd278a3db11cd8ef49858c30fe5
JuanAR05 <amadorrubioj@gmail.com>
2020-02-06 13:05:44 +0100

    Terminada la tarea 3 close #3

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 
 public class Principal {
-	//Fin de la tarea 3
+	//Final de la tarea 3
 	public static void main(String[] args) {
 		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
 		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
